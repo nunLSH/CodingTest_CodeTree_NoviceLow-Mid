cff1fd5122536f3404976fff30f3deb75085b852
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-04 18:07:11 +0900

    Title: 일의 자리 배열, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250104/일의 자리 배열/array-with-units.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] array = new int[10];
+        array[0] = sc.nextInt();
+        array[1] = sc.nextInt();
+
+        for (int i = 2; i < 10; i++){
+            array[i] = (array[i-1] + array[i-2]) % 10;
+        }
+
+        for (int i = 0; i < 10; i++)
+            System.out.print(array[i]+" ");
+    }
+}
