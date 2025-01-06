a46998d9711504f6cec27d8e9621a529892d8434
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-06 17:08:19 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 134ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250106/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] carPrice = new int[n];
+        
+        for (int i = 0; i < n; i++)
+            carPrice[i] = sc.nextInt();
+
+        int maxProfit = carPrice[1]-carPrice[0];
+
+        for (int i = 0; i < n; i++){
+            for (int j = 1; j < n; j++){
+                if (j > i && carPrice[j] - carPrice[i] > maxProfit){
+                    maxProfit = carPrice[j] - carPrice[i];
+                }
+            }
+        }
+        if (maxProfit <= 0)
+            System.out.print(0);
+        else
+            System.out.print(maxProfit);
+    }
+}
