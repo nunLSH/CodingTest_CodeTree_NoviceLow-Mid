73a73e9deb4378da0721d7f3985fd5ed957c7410
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-06 16:12:32 +0900

    Title: 500 근처의 수, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250106/500 근처의 수/near-500.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] array = new int[10];
+
+        for (int i = 0; i < 10; i++)
+            array[i] = sc.nextInt();
+        
+        int min = array[0], max = array[0];
+
+        // 배열의 최소, 최대
+        for (int i = 0; i < 10; i++){
+            if (array[i] > max)
+                max = array[i];
+            if (array[i] < min)
+                min = array[i];
+        }
+
+        // 배열의 최소값은 500미만 중 가장 큰 수에 할당
+        // 배열의 최대값은 500초과 중 가장 작은 수에 할당
+        int s_max = min, l_min = max;
+
+        for (int i = 0; i < 10; i++){
+            if (array[i] < 500){
+                if (array[i] > s_max)
+                    s_max = array[i];
+            } else if (array[i] > 500){
+                if (array[i] < l_min)
+                    l_min = array[i];
+            }
+        }
+        System.out.printf("%d %d", s_max, l_min);
+    }
+}
