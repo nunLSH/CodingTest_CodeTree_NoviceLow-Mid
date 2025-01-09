cdf30277993df7923519f3d760f6541686df09e7
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-10 01:31:44 +0900

    Title: 격자 반대로 채우기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 250110/격자 반대로 채우기/grid-anti-fill.java
+++ 250110/격자 반대로 채우기/grid-anti-fill.java
@@ -8,11 +8,11 @@ public class Main {
         int num = n*n;
 
         for (int i = 0; i < n; i++){
-            if (i % 2 == 0){
+            if (n % 2 == 0 && i % 2 == 0 || n % 2 != 0 && i % 2 != 0){
                 for (int j = n-1; j >= 0; j--)
                     array[j][i] = num--;
             }
-            if (i % 2 != 0){
+            if (n % 2 == 0 && i % 2 != 0 || n % 2 != 0 && i % 2 == 0){
                 for (int j = 0; j < n; j++)
                     array[j][i] = num--;
             }
