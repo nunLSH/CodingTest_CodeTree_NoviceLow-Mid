93108ab429c2693c5014a0ddc0adc0b011a34e87
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-14 11:08:30 +0900

    Title: 문자열 나누기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250114/문자열 나누기/divide-string.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] str = new String[n];
+        String s = "";
+
+        for (int i = 0; i < n; i++){
+            str[i] = sc.next();
+            s += str[i];
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (cnt % 5 == 0 && cnt != 0)
+                System.out.println();
+            System.out.print(s.charAt(i));
+            cnt++;
+        }
+    }
+}
