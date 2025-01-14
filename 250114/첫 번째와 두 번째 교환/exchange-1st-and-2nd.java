6d153115716c315009a1ccf5741ea3ec0f839150
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-14 13:00:40 +0900

    Title: 첫 번째와 두 번째 교환, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250114/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        char[] arr = s.toCharArray();
+
+        char c1 = s.charAt(0);
+        char c2 = s.charAt(1);
+
+        for (int i = 0; i < s.length(); i++){
+            if (arr[i] == c1){
+                arr[i] = c2;
+                continue;
+            }
+            if (arr[i] == c2){
+                arr[i] = c1;
+                continue;
+            }
+        }
+        System.out.print(String.valueOf(arr));
+    }
+}
