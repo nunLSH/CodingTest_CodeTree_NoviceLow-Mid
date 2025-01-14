9b493e9f38cd18c98330d605a7ee1252d56e3964
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-14 19:55:38 +0900

    Title: Run Length 인코딩, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250114/Run Length 인코딩/run-length-encoding.java
+++ 250114/Run Length 인코딩/run-length-encoding.java
@@ -5,43 +5,50 @@ public class Main {
 
         String str = sc.nextLine();
         int len = str.length();
-        char[] charArr = new char[len];
-        int[] num = new int[len];
+        char[] charArr = new char[1000];
+        int[] num = new int[1000];
         int cnt = 1, start = 1;
 
-        for (int i = 0; i < len; i++){
+        for (int i = 0; i < 1000; i++){
             if (start == len)
                 break;
-            for (int j = start; j < len; j++){
-                if (str.charAt(j-1) == str.charAt(j)){
-                    if (j == len-1) {
-                        cnt++;
+            else {
+                for (int j = start; j < len; j++){
+                    if (str.charAt(j-1) == str.charAt(j)){
+                        if (j == len-1) {
+                            cnt++;
+                            charArr[i] = str.charAt(j-1);
+                            num[i] = cnt;
+                            start += num[i];
+                            break;
+                        } else {
+                            cnt++;
+                        }
+                    } else {
                         charArr[i] = str.charAt(j-1);
                         num[i] = cnt;
                         start += num[i];
+                        cnt = 1;
+
+                        if (j == len -1) {
+                            charArr[i+1] = str.charAt(j);
+                            num[i+1] = cnt;
+                            start += num[i+1];
+                        }
                         break;
-                    } else {
-                        cnt++;
                     }
-                } else {
-                    charArr[i] = str.charAt(j-1);
-                    num[i] = cnt;
-                    start += num[i];
-                    cnt = 1;
-                    break;
                 }
             }
         }
         
         String result = "";
-        for (int i = 0; i < len; i++){
+        for (int i = 0; i < 1000; i++){
             if (num[i] != 0) {
                 result += charArr[i];
                 result += num[i];
             } else 
                 break;
         }
-
         System.out.println(result.length());
         System.out.println(result);
     }
