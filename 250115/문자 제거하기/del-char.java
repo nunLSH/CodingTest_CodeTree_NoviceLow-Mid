73d581b88c2f9341b6be7a068832e019c31aa81b
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-15 17:44:53 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250115/문자 제거하기/del-char.java
+++ 250115/문자 제거하기/del-char.java
@@ -6,17 +6,16 @@ public class Main {
         String str = sc.next();
         int len = str.length();
 
-        while (len != 1) {
+        while (len > 1) {
             int n = sc.nextInt();
 
-            if (n == 0) 
-                str = str.substring(1);
-            else if (n > 0 && n < len)
-                str = str.substring(0, n)+str.substring(n+1);
-            else if (n >= len)
-                str = str.substring(0, len-1);
+      
+            if (n >= len)
+                a = len -1;
 
-            len = str.length();
+            str = str.substring(0, n)+str.substring(n+1);
+            len--;
+
             System.out.println(str);
         }
     }
