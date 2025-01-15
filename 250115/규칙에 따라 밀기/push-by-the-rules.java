1f37d3d6454c8ef448614b6546819138dbc11cd3
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-15 20:18:55 +0900

    Title: 규칙에 따라 밀기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 250115/규칙에 따라 밀기/push-by-the-rules.java
+++ 250115/규칙에 따라 밀기/push-by-the-rules.java
@@ -4,15 +4,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String s = sc.next();
-        int s_len = s.length();
+        int len = s.length();
         String commend = sc.next();
-        int len = commend.length();
 
-        for (int i = 0; i < len; i++){
+        for (int i = 0; i < commend.length(); i++){
             if (commend.charAt(i) == 'L'){
-                s = s.substring(1, s_len)+s.substring(0,1);
+                s = s.substring(1, len)+s.substring(0,1);
             } else {
-                s = s.substring(s_len-1)+s.substring(0, s_len-1);
+                s = s.substring(len-1)+s.substring(0, len-1);
             }
         }
         System.out.print(s);
