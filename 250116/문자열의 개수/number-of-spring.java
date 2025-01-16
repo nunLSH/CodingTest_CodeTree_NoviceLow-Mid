699b1cb3e1552bd813129c7e21903119f7ed4acf
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-16 21:44:00 +0900

    Title: 문자열의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250116/문자열의 개수/number-of-spring.java
+++ 250116/문자열의 개수/number-of-spring.java
@@ -6,19 +6,16 @@ public class Main {
         int cnt = 0;
         String[] arr = new String[201];
         for (int i = 1; i < 201; i++) {
-            String s = sc.next();
+            arr[i] = sc.next();
 
             if (s.equals("0")) 
                 break;
             cnt++;
-            
-            arr[i] = s;
         }
 
         System.out.println(cnt);
-        for (int i = 1; i < cnt+1; i++){
-            if (i % 2 != 0)
-                System.out.println(arr[i]);
+        for (int i = 0; i < cnt; i += 2){
+            System.out.println(arr[i]);
         }
     }
 }
