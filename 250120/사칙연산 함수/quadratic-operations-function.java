df2f098753f85d68927a25fb2a737edba97dea86
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-20 17:51:24 +0900

    Title: 사칙연산 함수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250120/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+
+    public static int calculate(int a, char o, int c){
+        if (o == '+')
+            return a +c;
+        else if (o == '-')
+            return a - c;
+        else if (o == '*')
+            return a * c;
+        else if (o == '/')
+            return a / c;
+        else
+            return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        char o = sc.next().charAt(0);
+        int c = sc.nextInt();
+
+        if (calculate(a, o, c) == -1)
+            System.out.print("False");
+        else
+            System.out.print(Integer.toString(a) + " " + o + " " + Integer.toString(c) + " = " +calculate(a, o, c));
+    }
+}
