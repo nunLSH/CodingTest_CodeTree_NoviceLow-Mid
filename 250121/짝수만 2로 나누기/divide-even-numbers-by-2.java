5be2a20023ea8c3aa413618e0da315b4af0953b4
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-21 23:41:25 +0900

    Title: 짝수만 2로 나누기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 250121/짝수만 2로 나누기/divide-even-numbers-by-2.java
+++ 250121/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class Main {
+    public static int n;
+
     public static void changeArray(int[] arr){
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < n; i++){
 
             if (arr[i] % 2 == 0){
                 arr[i] /= 2;
@@ -12,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        n = sc.nextInt();
         int[] arr = new int[n];
         
         for (int i = 0; i < n; i++)
