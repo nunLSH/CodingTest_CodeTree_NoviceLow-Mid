08f95caec7d06e22335dca360e8b2552a34aadab
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-21 18:23:55 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 250121/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 250121/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -1,8 +1,21 @@
 import java.util.Scanner;
 public class Main {
-    public static String isSubsequence(String s1, String s2){
-        if (s1.contains(s2))
-            return "Yes";
+    public static String isSubsequence(int[] arr1, int[] arr2){
+        int len = arr2.length;
+        int[] newArr = new int[len];
+        for (int i = 0; i < arr1.length-len+1; i++){
+            for (int j = 0; j < len; j++){
+                newArr[j] = arr1[i+j];
+            }
+
+            int cnt = 0;
+            for (int k = 0; k < len; k++){
+                if (newArr[k] == arr2[k])
+                    cnt++;
+            }
+            if (cnt == len)
+                return "Yes";
+        }
         
         return "No";
     }
@@ -12,19 +25,16 @@ public class Main {
 
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
-        String s1 = "";
-        String s2 = "";
+        int[] arr1 = new int[n1];
+        int[] arr2 = new int[n2];
 
-        for (int i = 0; i < n1; i++){
-            int a = sc.nextInt();
-            s1 += Integer.toString(a)+" ";
-        }
 
-        for (int i = 0; i < n2; i++){
-            int b = sc.nextInt();
-            s2 += Integer.toString(b)+" ";
-        }
+        for (int i = 0; i < n1; i++)
+            arr1[i] = sc.nextInt();
 
-        System.out.print(isSubsequence(s1, s2));
+        for (int i = 0; i < n2; i++)
+            arr2[i] = sc.nextInt();
+
+        System.out.print(isSubsequence(arr1, arr2));
     }
 }
