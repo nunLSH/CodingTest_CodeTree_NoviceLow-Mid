cf7129c6a1ec181a8f017975a0f7d3ee6d6aa5b3
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-21 23:22:29 +0900

    Title: 두 정수 값 교환하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250121/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class IntWrapper {
+    int value;
+
+    public IntWrapper(int value) {
+        this.value = value;
+    }
+}
+
+public class Main {
+    public static void swap(IntWrapper a, IntWrapper b){
+        int temp = a.value;
+        a.value = b.value;
+        b.value = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        IntWrapper a = new IntWrapper(n);
+        IntWrapper b = new IntWrapper(m);
+
+        swap(a, b);
+        System.out.print(a.value + " " + b.value);
+    }
+}
