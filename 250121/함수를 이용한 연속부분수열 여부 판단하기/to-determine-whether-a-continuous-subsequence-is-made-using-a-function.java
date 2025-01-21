fac0a26cb1df93caa8a059f083c5b45927287e50
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-21 17:03:55 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250121/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static String isSubsequence(String s1, String s2){
+        if (s1.contains(s2))
+            return "Yes";
+        
+        return "No";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        String s1 = "";
+        String s2 = "";
+
+        for (int i = 0; i < n1; i++){
+            int a = sc.nextInt();
+            s1 += Integer.toString(a);
+        }
+
+        for (int i = 0; i < n2; i++){
+            int b = sc.nextInt();
+            s2 += Integer.toString(b);
+        }
+
+        System.out.print(isSubsequence(s1, s2));
+    }
+}
