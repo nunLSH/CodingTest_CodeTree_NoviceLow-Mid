788cb3d93ab33246277d2bb28412b12bb50ec849
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-24 15:30:43 +0900

    Title: 1이 되는 순간까지, Time: 430ms, Memory: 76MB, Status: Runtime Error - CodeTree

---
+++ 250124/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static int sumEven(int n){
+        if (n / 2 == 1 || n / 3 == 1)
+            return 1;
+        
+        if (n % 2 == 0)
+            n /= 2;
+        else
+            n /= 3;
+
+        return 1 + sumEven(n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.print(sumEven(n));
+    }
+}
