b0170bf0d852b726a95cccb211e828b9afb69e1e
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-24 15:33:04 +0900

    Title: 1이 되는 순간까지, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 250124/1이 되는 순간까지/until-the-moment-I-reach-one.java
+++ 250124/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class Main {
     public static int sumEven(int n){
-        if (n / 2 == 1 || n / 3 == 1)
-            return 1;
+        if (n == 1)
+            return 0;
         
         if (n % 2 == 0)
             n /= 2;
