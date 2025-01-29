224f799f6894c681e0cb0f046d89be11071db19c
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-29 23:40:45 +0900

    Title: 상품 코드, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 250129/상품 코드/product-code.java
+++ 250129/상품 코드/product-code.java
@@ -4,25 +4,21 @@ class Product {
     String name;
     int code;
 
-    public Product(){
-        this.name = "";
-        this.code = 0;
+    public Product(String name, int code){
+        this.name = name;
+        this.code = code;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Product p1 = new Product();
-        p1.name = "codetree";
-        p1.code = 50;
+        Product p1 = new Product("codetree", 50);
 
         String name = sc.next();
         int code = sc.nextInt();
 
-        Product p2 = new Product();
-        p2.name = name;
-        p2.code = code;
+        Product p2 = new Product(name, code);
 
         System.out.println("product "+p1.code+" is "+p1.name);
         System.out.println("product "+p2.code+" is "+p2.name);
