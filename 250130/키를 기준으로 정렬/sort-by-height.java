5565d94c4a2cf84fe2d47c322c294f09a82e202b
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-01-30 23:23:31 +0900

    Title: 키를 기준으로 정렬, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250130/키를 기준으로 정렬/sort-by-height.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Student implements Comparable<Student> {
+    String name;
+    int height, weight;
+
+    public Student(String name, int height, int weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Student student) { // 국어 점수 기준 오름차순 정렬
+        return this.height - student.height;
+    }
+};
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+
+        for (int i = 0; i < n; i++){
+            String name = sc.next();
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+
+            students[i] = new Student(name, height, weight);
+        }
+
+
+        Arrays.sort(students);
+
+        for(int i = 0; i < n; i++)
+            System.out.println(students[i].name + " " + students[i].height + " " + students[i].weight);
+    }
+}
+
