b6eda5b2b8381844e1ed664945c40720647c94e8
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-02-03 23:57:12 +0900

    Title: 십진수와 이진수 2, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 250203/십진수와 이진수 2/decimal-and-binary-number-2.java
+++ 250203/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String number = sc.next();
+        String binary = sc.next();
 
         int num = 0;
-        for (int i = 0; i < number.length(); i++){
-            num = num * 2 + (number.charAt(i) - '0');
+        for (int i = 0; i < binary.length(); i++){
+            num = num * 2 + (binary.charAt(i) - '0');
         }
         num *= 17;
 
