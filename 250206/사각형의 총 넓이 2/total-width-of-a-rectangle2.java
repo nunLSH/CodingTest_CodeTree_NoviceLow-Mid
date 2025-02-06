09cde05ac1c325ed151b9b380214aa8b4a8df984
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-02-06 23:36:59 +0900

    Title: 사각형의 총 넓이 2, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250206/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 250206/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Main {
+    public static final int OFFSET = 100;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -7,17 +8,17 @@ public class Main {
         int[][] square = new int[200][200];
 
         for (int i = 0; i < n; i++){
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
+            int x1 = sc.nextInt() + OFFSET;
+            int y1 = sc.nextInt() + OFFSET;
+            int x2 = sc.nextInt() + OFFSET;
+            int y2 = sc.nextInt() + OFFSET;
 
             for (int k = x1; k < x2; k++){
                 for (int j = y1; j < y2; j++)
                     square[k][j]++;
             }
         }
-        
+
         int area = 0;
         for (int i = 0; i < 200; i++){
             for (int j = 0; j < 200; j++){
