394e12fb8da5ee5a2173c00c434f8e27ecc52095
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-02-12 23:34:55 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 213ms, Memory: 27MB, Status: Passed - CodeTree

--- 250212/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 250212/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -1,66 +1,55 @@
 import java.util.Scanner;
 public class Main {
     public static final int OFFSET = 1000;
+    public static final int MAX_R = 2000;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] square = new int[2000][2000];
+        int[] x1 = new int[2];
+        int[] y1 = new int[2];
+        int[] x2 = new int[2];
+        int[] y2 = new int[2];
 
-        int x1 = sc.nextInt() + OFFSET;
-        int y1 = sc.nextInt() + OFFSET;
-        int x2 = sc.nextInt() + OFFSET;
-        int y2 = sc.nextInt() + OFFSET;
+        int[][] square = new int[2001][2001];
 
-        int x21 = sc.nextInt() + OFFSET;
-        int y21 = sc.nextInt() + OFFSET;
-        int x22 = sc.nextInt() + OFFSET;
-        int y22 = sc.nextInt() + OFFSET;
-
-        if (x21 <= x1 && y21 <= y1 && x2 <= x22 & y2 <= y22)
-            System.out.print(0);
-        else {
-            for (int i = x1; i < x2; i++){
-                for (int j = y1; j < y2; j++)
-                    square[i][j]++;
-            }
-
-            for (int i = x21; i < x22; i++){
-                for (int j = y21; j < y22; j++)
-                    square[i][j] = 2;
-            }
-
-            int minx = 0, miny = 0, cnt = 0;
-
-            for (int i = x1; i < x2; i++){
-                for (int j = y1; j < y2; j++){
-                    if (square[i][j] == 1){
-                        minx = i;
-                        miny = j;
-                        cnt = 1;
-                        break;
-                    }
-                }
-                if (cnt == 1)
-                    break;
-            }
-
-            int maxx = minx, maxy = miny;
-
-            for (int i = x1; i < x2; i++){
-                for (int j = y1; j < y2; j++){
-                    if (square[i][j] == 1){
-                        if (maxx < i)
-                            maxx = i;
-                        if (maxy < j)
-                            maxy = j;
-                    }
-                }
-            }
-            int width = maxx - minx + 1;
-            int height = maxy - miny + 1;
-        
-            System.out.print(width * height);
+        for(int i = 0; i < 2; i++) {
+            x1[i] = sc.nextInt();
+            y1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+            y2[i] = sc.nextInt();
+            
+            x1[i] += OFFSET;
+            y1[i] += OFFSET;
+            x2[i] += OFFSET;
+            y2[i] += OFFSET;
         }
+
+        for(int i = 0; i < 2; i++)
+            for(int x = x1[i]; x < x2[i]; x++)
+                for(int y = y1[i]; y < y2[i]; y++)
+                    square[x][y] = i + 1;
+
+        int minX = MAX_R, maxX = 0, minY = MAX_R, maxY = 0;
+        boolean firstRectExist = false;
+
+        for(int x = 0; x <= MAX_R; x++)
+            for(int y = 0; y <= MAX_R; y++)
+                if(square[x][y] == 1) {
+                    firstRectExist = true;
+                    minX = Math.min(minX, x);
+                    maxX = Math.max(maxX, x);
+                    minY = Math.min(minY, y);
+                    maxY = Math.max(maxY, y);
+                }
+        
+        int area = 0;
+
+        if (!firstRectExist)
+            area = 0;
+        else
+            area = (maxX - minX + 1) * (maxY - minY + 1);
+        
+        System.out.print(area);
     }
 }
