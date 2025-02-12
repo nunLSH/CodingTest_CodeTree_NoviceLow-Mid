3665c010243b5139e6f4bbc9ddc9ef65096d6c27
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-02-12 23:57:33 +0900

    Title: 신기한 타일 뒤집기, Time: 201ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 250212/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Main {
+    public static final int MAX = 200000;
+    public static int[] color = new int[MAX];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int current = MAX / 2;
+
+        for (int i = 0; i < n; i++){
+            int x = sc.nextInt();
+            char direction = sc.next().charAt(0);
+
+            if (direction == 'L'){
+                for (int j = current; j > current - x; j--)
+                    color[j] = 1;
+                current = current - x + 1;
+            } else {
+                for (int j = current; j < current + x; j++)
+                    color[j] = 2;
+                current = current + x - 1;
+            }
+        }
+
+        int cntW = 0, cntB = 0;
+        for (int i = 0; i < MAX; i++){
+            if (color[i] == 1)
+                cntW++;
+            if (color[i] == 2)
+                cntB++;
+        }
+
+        System.out.print(cntW + " " + cntB);
+    }
+}
