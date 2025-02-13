1abccf1a9594a23a300f5173cc9ba307fa5b68c9
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-02-13 20:49:40 +0900

    Title: 흰검 칠하기, Time: 204ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 250213/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Main {
+    public static final int MAX = 1000 * 100;
+    public static int[][] tiles = new int[3][2 * MAX];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cur = MAX;
+        for (int i = 0; i < n; i++){
+            int x = sc.nextInt();
+            char dir = sc.next().charAt(0);
+
+            if (dir == 'R') { // 검은색 1
+                for (int j = cur; j < cur + x; j++){
+                    if (tiles[0][j] == 3)
+                        continue;
+                    tiles[1][j]++;
+                    tiles[0][j] = 1;
+                    if (tiles[1][j] >= 2 && tiles[2][j] >= 2) {
+                        tiles[0][j] = 3;
+                        continue;
+                    }
+                }
+                cur = cur + x - 1;
+            } else { // 흰색 2
+                for (int j = cur; j > cur - x; j--){
+                    if (tiles[0][j] == 3)
+                        continue;
+                    tiles[2][j]++;
+                    tiles[0][j] = 2;
+                    if (tiles[1][j] >= 2 && tiles[2][j] >= 2) {
+                        tiles[0][j] = 3;
+                        continue;
+                    }
+                }
+                cur = cur - x + 1;
+            }
+        }
+
+        int cntW = 0, cntB = 0, cntG = 0;
+        for (int i = 0; i < 2 * MAX; i++){
+            if (tiles[0][i] == 3) // 회색
+                cntG++;
+            if (tiles[0][i] == 1) // 검은색
+                cntB++;
+            if (tiles[0][i] == 2) // 흰색
+                cntW++;
+        }
+        System.out.print(cntW+" "+cntB+" "+cntG);
+    }
+}
