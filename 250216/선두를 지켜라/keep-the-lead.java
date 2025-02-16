1a61d741bbb6a487facad272e1c47d6e8b686144
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-02-16 23:34:52 +0900

    Title: 선두를 지켜라, Time: 136ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250216/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Main {
+    public static final int MAX = 1000 * 1000;
+    public static int[] a = new int[MAX + 1];
+    public static int[] b = new int[MAX + 1];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int timeA = 1, timeB = 1;
+        int curA = 0, curB = 0;
+
+        for (int i = 0; i < n; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for (int j = timeA; j < timeA + t; j++){
+                curA += v;
+                a[j] = curA;
+            }
+            timeA += t;
+        }
+
+        for (int i = 0; i < m; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for (int j = timeB; j < timeB + t; j++){
+                curB += v;
+                b[j] = curB;
+            }
+            timeB += t;
+        }
+
+        int cnt = 0;
+        boolean aIsFirst = false;
+        for (int i = 1; i <= timeA; i++){
+            if (a[i] > b[i]){
+                if (aIsFirst == false)
+                    cnt++;
+                aIsFirst = true;
+            } else {
+                if (aIsFirst == true)
+                    cnt++;
+                aIsFirst = false;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
