4f8c01890e242dc98f70e85a5f1cacd6f352e371
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-02-20 23:02:49 +0900

    Title: 좌우로 움직이는 로봇, Time: 829ms, Memory: 58MB, Status: Passed - CodeTree

--- 250220/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250220/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -24,10 +24,6 @@ public class Main {
             }
         }
 
-        for (int j = timeA; j < MAX + 1; j++){
-                a[j] = a[timeA - 1];
-        }
-
         for (int i = 0; i < m; i++){
             int t = sc.nextInt();
             char d = sc.next().charAt(0);
@@ -41,15 +37,23 @@ public class Main {
             }
         }
 
-        for (int j = timeB; j < MAX + 1; j++){
-            b[j] = b[timeB - 1];
+        int maxTime = 0;
+        if (timeA < timeB) {
+            for (int i = timeA; i < timeB; i++){
+                a[i] = a[i - 1];
+            }
+            maxTime = timeB;
+        } else if (timeB < timeA) {
+            for (int i = timeB; i < timeA; i++){
+                b[i] = b[i - 1];
+            }
+            maxTime = timeA;
         }
         
         int cnt = 0;
-        for (int i = 2; i < MAX+1; i++){
-            if (a[i] == b[i]){
-                if (a[i-1] != b[i-1])
-                    cnt++;
+        for (int i = 2; i < maxTime; i++){
+            if (a[i] == b[i] && a[i-1] != b[i-1]){
+                cnt++;
             }
         }
         System.out.print(cnt);
