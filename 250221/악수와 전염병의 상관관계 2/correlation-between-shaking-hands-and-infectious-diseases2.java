5a9370f2c75f6b53df2eae40adf150fd909ea855
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-02-21 00:45:34 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 109ms, Memory: 10MB, Status: Passed - CodeTree

--- 250221/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 250221/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class Intro implements Comparable<Intro>{
-    int time, x, y;
+class Shake implements Comparable<Shake>{
+    int time, person1, person2;
 
-    public Intro(int time, int x, int y){
+    public Shake(int time, int person1, int person2){
         this.time = time;
-        this.x = x;
-        this.y = y;
+        this.person1 = person1;
+        this.person2 = person2;
     }
 
     @Override
-    public int compareTo(Intro intro) {
-        return this.time - intro.time;
+    public int compareTo(Shake shake) {
+        return this.time - shake.time;
     }
 }
 
@@ -21,48 +21,41 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[][] devs = new int[2][n+1]; // 개발자 배열 (0:감염여부, 1:감염 횟수)
+        int[][] devs = new int[2][n+1]; // 개발자 배열 (0행:감염여부, 1행:감염 횟수)
         int k = sc.nextInt();
         int p = sc.nextInt();
         int t = sc.nextInt();
-        Intro[] intro = new Intro[t];
+        Shake[] shake = new Shake[t];
 
         devs[0][p] = 1; // 감염된 개발자
 
         // 악수 정보 시간 순으로 정렬
         for (int i = 0; i < t; i++){
             int time = sc.nextInt();
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+            int person1 = sc.nextInt();
+            int person2 = sc.nextInt();
 
-            intro[i] = new Intro(time, x, y);
+            shake[i] = new Shake(time, person1, person2);
         }
-        Arrays.sort(intro);
+        Arrays.sort(shake, 0, t);
         
         // 감염여부 판단
         for (int i = 0; i < t; i++){
-            int xDevs = intro[i].x;
-            int yDevs = intro[i].y;
-            // x나 y가 감염된 개발자이면서 감염 가능한 횟수도 남아있을 경우
-            if (devs[0][xDevs] == 1 && devs[1][xDevs] < k) {
-                if (devs[0][yDevs] == 1 && devs[1][yDevs] < k) {
-                    devs[0][xDevs] = 1;
-                    devs[0][yDevs] = 1;
-                    devs[1][yDevs]++; // 전염 횟수 중가
-                }
-                devs[0][xDevs] = 1;
-                devs[0][yDevs] = 1;
-                devs[1][xDevs]++; // 전염 횟수 중가
-            } else if (devs[0][yDevs] == 1 && devs[1][yDevs] < k) {
-                if (devs[0][xDevs] == 1 && devs[1][xDevs] < k) {
-                    devs[0][xDevs] = 1;
-                    devs[0][yDevs] = 1;
-                    devs[1][xDevs]++;
-                }
-                devs[0][xDevs] = 1;
-                devs[0][yDevs] = 1;
-                devs[1][yDevs]++; // 전염 횟수 중가
-            }
+            int target1 = shake[i].person1;
+            int target2 = shake[i].person2;
+
+            // 감염되어 있을 경우 악수 횟수 증가
+            if (devs[0][target1] == 1)
+                devs[1][target1]++;
+            if (devs[0][target2] == 1)
+                devs[1][target2]++;
+
+            // target1이 감염되어 있고, k번 이하로 악수한 경우
+            if (devs[0][target1] == 1 && devs[1][target1] <= k)
+                devs[0][target2] = 1;
+            // target2가 감염되어 있고, k번 이하로 악수한 경우
+            if (devs[0][target2] == 1 && devs[1][target2] <= k)
+                devs[0][target1] = 1;
         }
             
         // 감염여부 출력
