75ad4c314e65eef336805cd85327cfa4e914aa8d
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-02-25 23:06:10 +0900

    Title: 1이 3개 이상 있는 위치, Time: 239ms, Memory: 18MB, Status: Passed - CodeTree

--- 250225/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ 250225/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -1,15 +1,34 @@
 import java.util.Scanner;
 public class Main {
+    public static final int MAX_N = 100;
+    public static final int DIR_NUM = 4;
+
+    public static int n;
+    public static int[][] arr = new int[MAX_N][MAX_N];
+
+    public static int[] dx = {0, 0, -1, 1};
+    public static int[] dy = {1, -1, 0, 0};
+
     public static boolean inRange(int n, int x, int y) {
         return (0 <= x && x < n && 0 <= y && y < n);
     }
 
+    public static int adjacentCnt(int x, int y){
+        int cnt = 0;
+            for (int i = 0; i < 4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (inRange(n, nx, ny) && arr[nx][ny] == 1)
+                    cnt++;
+            }
+        
+        return cnt;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
+        n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -20,15 +39,8 @@ public class Main {
         int ans = 0;
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                int cnt = 0;
-                for (int dirNum = 0; dirNum < 4; dirNum++){
-                    int nx = i + dx[dirNum];
-                    int ny = j + dy[dirNum];
-
-                    if (inRange(n, nx, ny) && arr[nx][ny] == 1)
-                        cnt++;
-                }
-                if (cnt >= 3)
+            
+                if (adjacentCnt(i, j) >= 3)
                     ans++;
             }
         }
