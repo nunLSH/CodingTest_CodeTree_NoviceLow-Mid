725b1219dc5b99ae1a0e7b9098a6e4d09fd663d2
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-02-27 00:20:56 +0900

    Title: 작은 구슬의 이동, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250227/작은 구슬의 이동/small-marble-movement.java
+++ 250227/작은 구슬의 이동/small-marble-movement.java
@@ -4,40 +4,44 @@ public class Main {
     public static int dx[] = new int[]{0, 1, -1, 0};
     public static int dy[] = new int[]{-1, 0, 0, 1};
 
-    public static int N;
+    public static int n;
 
     public static boolean inRange(int x, int y) {
-        return (1 <= x && x <= N && 1 <= y && y <= N);
+        return (1 <= x && x <= n && 1 <= y && y <= n);
+    }
+
+    public static int getDir(int dir){
+        if (D.equals("U")) 
+            return 0;
+        else if (D.equals("R"))
+            return 1;
+        else if (D.equals("L"))
+            return 2;
+        else 
+            return 3;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
+
+        n = sc.nextInt();
         int T = sc.nextInt();
         int R = sc.nextInt();
         int C = sc.nextInt();
         String D = sc.next();
 
-        int dirNum;
+        int dirNum = getDir(D);
 
-        if (D.equals("U")) 
-            dirNum = 0;
-        else if (D.equals("R"))
-            dirNum = 1;
-        else if (D.equals("L"))
-            dirNum = 2;
-        else 
-            dirNum = 3;
-
-        for (int i = 0; i < T; i++){
+        while (t-- > 0){
             int nx = C + dx[dirNum], ny = R + dy[dirNum];
                 
             if (inRange(nx, ny)) {
-                C += dx[dirNum];
-                R += dy[dirNum];
-            } else if (!inRange(nx, ny))
+                C += nx;
+                R += ny
+            } else 
                 dirNum = 3 - dirNum;
         }
+
         System.out.print(R + " " + C);
     }
 }
