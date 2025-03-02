7083176aae27ef32e4ad5c55d47ba945ff1f39eb
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-03-02 19:29:57 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 250302/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
+++ 250302/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
@@ -9,7 +9,6 @@ public class Main {
     public static int[] dy = new int[]{1, 0, -1, 0};
 
     public static int x = 1, y = 1, dir = 0;
-    public static char c = 'A';
 
     public static boolean inRange(int x, int y){
         return (0 < x && x <= n && 0 < y && y <= m);
@@ -19,25 +18,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
+
+        arr[x][y] = 'A';
         
-        for (int i = 1; i <= n * m; i++){
+        for (int i = 1; i < n * m; i++){
             int nx = x + dx[dir], ny = y + dy[dir];
 
-            if (i != 1) {
-                if (inRange(nx, ny) && arr[nx][ny] == 0){
-                    x = nx;
-                    y = ny;
-                } else {
-                    dir = (dir + 1) % 4;
-                    x = x + dx[dir];
-                    y = y + dy[dir];
-                }
+            if (inRange(nx, ny) && arr[nx][ny] == 0){
+                x = nx;
+                y = ny;
+            } else {
+                dir = (dir + 1) % 4;
+                x = x + dx[dir];
+                y = y + dy[dir];
             }
             
-            arr[x][y] = c++;
-
-            if (arr[x][y] == 'Z')
-                c = 'A';
+            arr[x][y] = (char)(i % 26 + 'A');
         }
 
         for (int i = 1; i <= n; i++){
