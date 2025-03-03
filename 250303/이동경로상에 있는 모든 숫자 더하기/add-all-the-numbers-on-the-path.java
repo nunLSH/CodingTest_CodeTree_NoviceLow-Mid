c35c14e918edbbcafdf972f049c2ed74b1b07258
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-03-03 16:25:14 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 262ms, Memory: 14MB, Status: Runtime Error - CodeTree

---
+++ 250303/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX = 99;
+    public static int n;
+
+    public static int[] dx = new int[]{0, 1, 0, -1};
+    public static int[] dy = new int[]{1, 0, -1, 0};
+
+    public static int x, y, dir = 3;
+
+    public static int getDir(int cmd){
+        if (cmd == 'L')
+            dir = (dir + 3) % 4;
+        else if (cmd == 'R')
+            dir = (dir + 1) % 4;
+        
+        return dir;
+    }
+
+    public static boolean inRange(int x, int y){
+        return (0 < x && x <= n && 0 < y && y < n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int t = sc.nextInt();
+        String commands = sc.next();
+
+        // 격자 채우기
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        x = n / 2;
+        y = n / 2;
+        int sum = board[x][y];
+        // 명령에 따라 움직이기
+        for (int i = 0; i < t; i++){
+            char cmd = commands.charAt(i);
+            getDir(cmd);
+
+            int nx = x + dx[dir], ny = y + dy[dir];
+            if (inRange(nx, ny)){
+                x = nx;
+                y = ny;
+                sum += board[nx][ny];
+            }
+        }
+
+        System.out.print(sum);
+    }
+}
