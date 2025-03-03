0b541cda3298665a66b3f09debbebebbc2bf6828
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-03-03 18:33:58 +0900

    Title: 모이자, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 250303/모이자/gather.java
+++ 250303/모이자/gather.java
@@ -2,27 +2,24 @@ import java.util.Scanner;
 public class Main {
     public static int n;
     public static final int MAX = Integer.MAX_VALUE;
-    public static int[] home = new int[101];
+    public static int[] a = new int[101];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
-        for (int i = 1; i <= n; i++){
-            int a = sc.nextInt();
-            home[i] = a;
-        }
-
+        for (int i = 1; i <= n; i++)
+            a[i] = sc.nextInt();
+        
         int minDis = MAX;
         for (int i = 1; i <= n; i++){
             int sumDis = 0;
 
             for (int j = 1; j <= n; j++){
-                sumDis += Math.abs((j - i) * home[j]);
+                sumDis += Math.abs((j - i) * a[j]);
             }
 
-            if (sumDis < minDis)
-                minDis = sumDis;
+            minDis = Math.min(minDis, sumDis);
         }
         System.out.print(minDis);
     }
