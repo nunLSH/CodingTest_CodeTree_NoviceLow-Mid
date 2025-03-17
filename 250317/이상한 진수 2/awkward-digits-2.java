48d8451e49573c90f80b369ca9939e5fb5da1c60
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-03-17 23:58:05 +0900

    Title: 이상한 진수 2, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250317/이상한 진수 2/awkward-digits-2.java
+++ 250317/이상한 진수 2/awkward-digits-2.java
@@ -5,19 +5,37 @@ public class Main {
 
         String a = sc.next();
         int len = a.length();
-        int n = 0, max = 0;
+        int max = 0;
 
-        for (int i = 1; i < len; i++){
+        for (int i = 0; i < len; i++){
             String newA = "";
-            if (a.charAt(i) == '1')
-                newA = a.substring(0, i) + '0' + a.substring(i+1);
-            else 
-                newA = a.substring(0, i) + '1' + a.substring(i+1);
-                
+            if (a.charAt(i) == '1'){
+                if (i != len - 1)
+                    newA = a.substring(0, i) + '0' + a.substring(i+1);
+                else 
+                    newA = a.substring(0, i) + '0';
+            } else {
+                if (i != len - 1)
+                    newA = a.substring(0, i) + '1' + a.substring(i+1);
+                else 
+                    newA = a.substring(0, i) + '1';
+            }
+
+            int n = 0, cnt = 0;
             for (int j = newA.length() - 1; j >= 0; j--){
-                n += (newA.charAt(j) - '0') * 2;
+                if (newA.charAt(j) == '1'){
+                    cnt = 1;
+                    if (j == newA.length() - 1)
+                        cnt *= 1;
+                    else {
+                        for (int k = 0; k < newA.length() - j - 1; k++)
+                            cnt *= 2;
+                    }
+                }
+                n += cnt;
             }
             max = Math.max(n, max);
+            // System.out.println();
         }
         System.out.println(max);
     }
