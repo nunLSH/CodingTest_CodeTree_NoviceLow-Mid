9df0b2d372c4ba1c1bc6299d0d7a6e950f45ec89
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-03-20 00:43:55 +0900

    Title: 원 모양으로 되어있는 방, Time: 207ms, Memory: 13MB, Status: Passed - CodeTree

--- 250320/원 모양으로 되어있는 방/a-room-in-a-circle.java
+++ 250320/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -9,27 +9,39 @@ public class Main {
             num_people[i] = sc.nextInt();
         }
 
+        // 해설
         int minDis = Integer.MAX_VALUE;
         for (int i = 1; i <= n; i++){
-            int sumDis = 0;
-            if (i == 1){
-                for (int j = 1; j <= n; j++){
-                    sumDis += num_people[j] * Math.abs(j - i);
-                }
-            } else if (i == n) {
-                for (int j = 1; j < n; j++){
-                    sumDis += num_people[j] * j;
-                }
-            } else {
-                for (int j = i; j <= n; j++){
-                    sumDis += num_people[j] * Math.abs(j - i);
-                }
-                for (int j = 1; j < i; j++){
-                    sumDis += num_people[j] * Math.abs(n + j - i);
-                }
-            } 
-            minDis = Math.min(minDis, sumDis);
+            int sumDist = 0;
+            for (int j = 1; j <= n; j++){
+                int dist = (j + n - i) % n;
+                sumDist += dist * num_people[j];
+            }
+               minDis = Math.min(minDis, sumDist);
         }
         System.out.println(minDis);
+
+        // int minDis = Integer.MAX_VALUE;
+        // for (int i = 1; i <= n; i++){
+        //     int sumDis = 0;
+        //     if (i == 1){
+        //         for (int j = 1; j <= n; j++){
+        //             sumDis += num_people[j] * Math.abs(j - i);
+        //         }
+        //     } else if (i == n) {
+        //         for (int j = 1; j < n; j++){
+        //             sumDis += num_people[j] * j;
+        //         }
+        //     } else {
+        //         for (int j = i; j <= n; j++){
+        //             sumDis += num_people[j] * Math.abs(j - i);
+        //         }
+        //         for (int j = 1; j < i; j++){
+        //             sumDis += num_people[j] * Math.abs(n + j - i);
+        //         }
+        //     } 
+        //     minDis = Math.min(minDis, sumDis);
+        // }
+        // System.out.println(minDis);
     }
 }
