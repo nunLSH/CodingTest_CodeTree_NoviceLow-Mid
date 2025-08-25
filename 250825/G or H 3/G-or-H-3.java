3fe21187ee2bc6ff0f4d659bfdbdeb1f1f6b38bb
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-08-25 23:25:18 +0900

    Title: G or H 3, Time: 196ms, Memory: 10MB, Status: Passed - Codetree

--- 250825/G or H 3/G-or-H-3.java
+++ 250825/G or H 3/G-or-H-3.java
@@ -13,13 +13,13 @@ public class Main {
         for (int i = 0; i < n; i++){
             int num = sc.nextInt();
             char c = sc.next().charAt(0);
-            maxNum = Math.max(num, maxNum);
+            // maxNum = Math.max(num, maxNum);
 
             arr[num] = c;
         } 
 
         int max = 0;
-        for (int i = 1; i <= maxNum-k; i++){
+        for (int i = 1; i <= 10000-k; i++){
             int sum = 0;
             for (int j = i; j <= i+k; j++){
                 if (arr[j] == 'G')
