4e45cc2a20aa17d148b81e9e99bf7611ed93429e
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-08-26 23:43:13 +0900

    Title: 특정 구간의 원소 평균값, Time: 151ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250826/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        int[] num = new int[n+1];
+        for (int i = 1; i <= n; i++)
+            num[i] = sc.nextInt();
+
+        int cnt = 0;
+        for (int i = 1; i <= n; i++){ // 크기
+            for (int k = 1; k < n+2 - i; k++){ // 시작점
+                double sum = 0;
+                for (int j = k; j < k+i; j++){  // 구간
+                    sum += num[j];
+                }
+                boolean isSame = false;
+                for (int t = k; t < k+i; t++){
+                    if (sum / i == (double) num[t]){
+                        isSame = true;
+                        break;
+                    }
+                }
+                if (isSame == true)
+                    cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
