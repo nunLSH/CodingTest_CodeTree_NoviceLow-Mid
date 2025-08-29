c562540ca3ba54d57fe845465570850a57407e96
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-08-29 22:55:27 +0900

    Title: G or H 2, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250829/G or H 2/G-or-H-2.java
+++ 250829/G or H 2/G-or-H-2.java
@@ -2,13 +2,13 @@ import java.util.*;
 public class Main {
     public static int n;
     public static final int MAX = 100;
+    public static int[] text = new int[MAX+1];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
         int maxPos = 0;
-        int[] text = new int[100+1];
         for (int i = 0; i < n; i++){
             int pos = sc.nextInt();
             char c = sc.next().charAt(0);
@@ -17,24 +17,25 @@ public class Main {
         }
 
         int sizeMax = 0;
-        for (int i = 0; i < maxPos; i++){ // 크기
-            for (int k = 1; k < maxPos+1 - i; k++){ // 시작점
-                int gCnt = 0, hCnt = 0, min = MAX, max = 0;
-                for (int j = k; j <= k + i; j++){ // 구간
+        for (int i = 0; i <= maxPos; i++){
+            for (int k = i+1; k <= maxPos; k++){
+                // 시작점 i, 종료점 k
 
-                    if (text[j] == 'G') {
+                if (text[i] == 0 || text[k] == 0)
+                    continue;
+
+                int gCnt = 0, hCnt = 0;    
+                for (int j = i; j <= k; j++){ // 구간
+
+                    if (text[j] == 'G') 
                         gCnt++;
-                        min = Math.min(min, j);
-                        max = Math.max(max, j);
-                    }
-                    else if (text[j] == 'H'){
+                    else if (text[j] == 'H')
                         hCnt++;
-                        min = Math.min(min, j);
-                        max = Math.max(max, j);
-                    }
+                
                 }
-                if (gCnt == 0 && hCnt > 0 || hCnt == 0 && gCnt > 0 || gCnt == hCnt){
-                    sizeMax = Math.max(max - min, sizeMax);
+
+                if (gCnt == 0 || hCnt == 0 || gCnt == hCnt){
+                    sizeMax = Math.max(k - i, sizeMax);
                 }
             }
         }
