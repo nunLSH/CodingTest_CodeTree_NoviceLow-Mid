10d96b6235f79e28de07a65c6e7be606d123663a
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-09-05 00:02:43 +0900

    Title: 개발팀의 능력, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250905/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] ability = new int[5];
+        int total = 0;
+
+        for (int i = 0; i < 5; i++) {
+            ability[i] = sc.nextInt();
+            total += ability[i];
+        }
+
+        int diffMin = Integer.MAX_VALUE;
+        boolean isPossible = false;
+
+        // 1명
+        for (int i = 0; i < 5; i++) {
+            int groupA = ability[i];
+
+            // 나머지 4명 중 2명 선택
+            for (int j = 0; j < 5; j++) {
+                if (j == i) continue;
+                for (int k = j + 1; k < 5; k++) {
+                    if (k == i) continue;
+
+                    int groupB = ability[j] + ability[k];
+
+                    // 나머지 2명 
+                    int groupC = total - groupA - groupB;
+                
+                    if (groupA == groupB || groupB == groupC || groupA == groupC)
+                        continue;
+                    int max = Math.max(groupA, Math.max(groupB, groupC));
+                    int min = Math.min(groupA, Math.min(groupB, groupC));
+
+                    diffMin = Math.min(diffMin, max - min);
+                    isPossible = true;
+                }
+            }
+        }
+
+        if (!isPossible) {
+            System.out.println(-1);
+        } else {
+            System.out.println(diffMin);
+        }
+    }
+}
