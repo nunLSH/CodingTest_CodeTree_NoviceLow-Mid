24c58e5b672ee841ff6737eb39f05dde79dd6464
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-09-04 23:56:50 +0900

    Title: 개발팀의 능력, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250904/개발팀의 능력/the-capabilities-of-the-development-team.java
+++ 250904/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] ability = new int[5];
+        int total = 0;
+        
         for (int i = 0; i < 5; i++) {
             ability[i] = sc.nextInt();
             total += ability[i];
