f77b3510e158ca490c6e7f314768f993fe2e474a
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-09-08 18:44:27 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250908/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
+++ 250908/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Main {
     public static int n;
-    public static final int MAX = 1000;
+    public static final int MAX = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,11 +13,9 @@ public class Main {
             dots[i][1] = sc.nextInt();
         }
 
-        int minSqDis = MAX * MAX * 2;
+        int minSqDis = MAX;
         for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if (i == j)
-                    continue;
+            for (int j = i + 1; j < n; j++){
                 
                 int dis = (dots[i][0] - dots[j][0]) * (dots[i][0] - dots[j][0]) 
                         + (dots[i][1] - dots[j][1]) * (dots[i][1] - dots[j][1]);
