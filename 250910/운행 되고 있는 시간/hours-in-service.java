411040611117f7a0abe6103bb922e23039e7a020
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-09-10 22:50:33 +0900

    Title: 운행 되고 있는 시간, Time: 156ms, Memory: 10MB, Status: Passed - Codetree

--- 250910/운행 되고 있는 시간/hours-in-service.java
+++ 250910/운행 되고 있는 시간/hours-in-service.java
@@ -20,13 +20,20 @@ public class Main {
         int maxWorkingTime = 0;
         for (int i = 0; i < n; i++){
             int workingTime = 0;
+
+            // 해고한 사람의 근무시간 제외하기
+            for (int k = a[i]; k < b[i]; k++)
+                works[k]--;
+
             for (int j = 1; j < MAX; j++){
-                for (int k = a[i]; k < b[i]; k++)
-                    works[k]--;
-                
                 if (works[j] != 0)
                     workingTime++;
             }
+
+            // 제외했던 근무시간 되돌리기
+            for (int k = a[i]; k < b[i]; k++)
+                works[k]++; 
+
             maxWorkingTime = Math.max(maxWorkingTime, workingTime);
         }
         System.out.print(maxWorkingTime);
