5f41179232b86183687ab8e6c80fa38a62bcd29e
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-09-16 00:19:59 +0900

    Title: 선분 3개 지우기, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 250916/선분 3개 지우기/remove-three-segments.java
+++ 250916/선분 3개 지우기/remove-three-segments.java
@@ -1,33 +1,13 @@
 
 import java.util.*;
+
 public class Main {
+    public static final int MAX_A = 100;
+    public static final int MAX_N = 10;
+    
     public static int n;
-    public static final int MAX = 100;
-    public static int[] lines = new int[MAX+1];
-    public static int[] a = new int[10];
-    public static int[] b = new int[10];
-
-    public static boolean countLines(int x, int y, int z){
-        for (int i = a[x]; i <= b[x]; i++) lines[i]--;
-        for (int i = a[y]; i <= b[y]; i++) lines[i]--;
-        for (int i = a[z]; i <= b[z]; i++) lines[i]--;
-        
-        for (int i = 0; i < MAX+1; i++){
-            if (lines[i] > 1) {
-                for (int j = a[x]; j <= b[x]; j++) lines[j]++;
-                for (int j = a[y]; j <= b[y]; j++) lines[j]++;
-                for (int j = a[z]; j <= b[z]; j++) lines[j]++;
-                
-                return false;
-            }
-        }
-
-        for (int i = a[x]; i <= b[x]; i++) lines[i]++;
-        for (int i = a[y]; i <= b[y]; i++) lines[i]++;
-        for (int i = a[z]; i <= b[z]; i++) lines[i]++;
-
-        return true;
-    }
+    public static int[] a = new int[MAX_N];
+    public static int[] b = new int[MAX_N];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -36,9 +16,6 @@ public class Main {
         for (int i = 0; i < n; i++){
             a[i] = sc.nextInt();
             b[i] = sc.nextInt();
-
-            for (int j = a[i]; j <= b[i]; j++)
-                lines[j]++;
         }
 
         int cnt = 0;
@@ -46,7 +23,22 @@ public class Main {
             for (int j = i+1; j < n; j++){
                 for (int k = j+1; k < n; k++){
             
-                    if (countLines(i, j, k))
+                    boolean overlap = false;
+                    int[] lines = new int[MAX_A + 1];
+
+                    for(int x = 0; x < n; x++) {
+                        if(x == i || x == j || x == k)
+                            continue;
+                        
+                        for(int y = a[x]; y <= b[x]; y++)
+                            lines[y]++;
+                    }
+
+                    for(int x = 0; x <= MAX_A; x++)
+                        if(lines[x] > 1)
+                            overlap = true;
+                    
+                    if(overlap == false)
                         cnt++;
                 }
             }
