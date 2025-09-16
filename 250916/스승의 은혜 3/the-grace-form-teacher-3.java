93395076134bf5fd07a25a96dfb04fd74c03226e
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-09-16 23:58:20 +0900

    Title: 스승의 은혜 3, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

--- 250916/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 250916/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -1,35 +1,52 @@
 import java.util.*;
+
 public class Main {
-    public static int n;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
+        int n = sc.nextInt();
         int b = sc.nextInt();
         int[] p = new int[n];
         int[] s = new int[n];
 
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             p[i] = sc.nextInt();
             s[i] = sc.nextInt();
         }
 
         int maxCnt = 0;
-        for (int i = 0; i < n; i++){ // 쿠폰을 사용할 학생
-            int sum = 0, cnt = 0;
-            for (int j = 0; j < n; j++){
+        // 쿠폰 사용 X
+        int[] baseCosts = new int[n];
+        for (int j = 0; j < n; j++) 
+            baseCosts[j] = p[j] + s[j];
+        Arrays.sort(baseCosts);
 
-                if (i == j)                
-                    sum += p[i] / 2 + s[i];
-                else 
-                    sum += p[i] + s[i];
-
-                if (sum > b)
-                    maxCnt = Math.max(cnt, maxCnt);
-                else 
-                    cnt++;
-            }
+        int sum = 0, cnt = 0;
+        for (int k = 0; k < n; k++) {
+            sum += baseCosts[k];
+            if (sum > b) break;
+            cnt++;
         }
+        maxCnt = Math.max(maxCnt, cnt);
+
+        // i번 학생에게 쿠폰
+        for (int i = 0; i < n; i++) {
+            int[] costs = new int[n];
+            for (int j = 0; j < n; j++) {
+                if (i == j) costs[j] = p[j] / 2 + s[j];
+                else costs[j] = p[j] + s[j];           
+            }
+            Arrays.sort(costs);
+
+            sum = 0;
+            cnt = 0;
+            for (int k = 0; k < n; k++) {
+                sum += costs[k];
+                if (sum > b) break;
+                cnt++;
+            }
+            maxCnt = Math.max(maxCnt, cnt);
+        }
+
         System.out.print(maxCnt);
     }
 }
